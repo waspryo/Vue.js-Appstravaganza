644eb91bf29f34a24822eb143ee63665fdb44aed
waspryo <wasp2123321@gmail.com>
2019-12-09 23:06:25 +0900

    up and down button for olayer 1, 2

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,15 @@
     <div class="card">
       <div class="card-body text-center">
         <h3>Player 1</h3>
-        <button @click="decreaseScore()" type="button" class="btn btn-danger">Down</button>
-        <span class="player-score">{{ player_1_score }}</span>
-        <button @click="increaseScore()" type="button" class="btn btn-success">Up</button>
+        <button @click="decreaseScore(player_1)" type="button" class="btn btn-danger">Down</button>
+        <span class="player-score">{{ player_1.score }}</span>
+        <button @click="increaseScore(player_1)" type="button" class="btn btn-success">Up</button>
+      </div>
+      <div class="card-body text-center">
+        <h3>Player 2</h3>
+        <button @click="decreaseScore(player_2)" type="button" class="btn btn-danger">Down</button>
+        <span class="player-score">{{ player_2.score }}</span>
+        <button @click="increaseScore(player_2)" type="button" class="btn btn-success">Up</button>
       </div>
     </div>
   </div>
@@ -17,16 +23,21 @@ export default {
   name: "app",
   data() {
     return {
-      player_1_score: 0
+      player_1: {
+        score: 0
+      },
+      player_2: {
+        score: 0
+      }
     };
   },
   methods: {
-    increaseScore() {
-      this.player_1_score += 1
+    increaseScore(player) {
+      player.score += 1
     },
-    decreaseScore() {
-      if (this.player_1_score > 0) {
-        this.player_1_score -= 1
+    decreaseScore(player) {
+      if (player.score > 0) {
+        player.score -= 1
       }
     },
   },
